2c44aadf6e89295f35f5b7f4a7d7ce5234effb4e
Kristoffer Jensen <krijen16@student.aau.dk>
2021-02-18 11:53:18 +0100

    update stringCopy

--- Examples/stringCopy/stringCopy.c
+++ Examples/stringCopy/stringCopy.c
@@ -7,33 +7,8 @@ void stringCopy1(char* dest, const char* src) ;
 int strlen2(const char* str) ;
 
 int main(int argc, char** argv){
-    unsigned short int nondet_ushortint();
-
-    unsigned short int size_dest = nondet_ushortint();
-    __CPROVER_assume(5 > size_dest && size_dest > 0); //size_dest = 4, 3, 2, 1
-    char dest[size_dest];
-    for(int j = 0; j < size_dest - 1; j++){
-        dest[j] = "a";
-    }
-    dest[size_dest-1]='\0';
-
-    unsigned short int size_src = nondet_ushortint(); 
-    __CPROVER_assume(5 > size_src && size_src > 1); //size_src = 4, 3, 2
-    char src[size_src];
-    for(int j = 0; j < size_src - 1; j++){
-        src[j] = "b";
-    }
-    src[size_src-1]='\0';
-
-    for(int i=0; i<size_dest-1; i++){
-        __CPROVER_assume(dest[i] != '\0');
-    }
-    __CPROVER_assume(size_dest >= size_src);
-
-    __CPROVER_printf("\nCASE");
-    __CPROVER_printf("Size of dest array: %d", (size_dest));
-    __CPROVER_printf("Size of src array: %d\n", (size_src));
-
+    char dest[] = "12345";
+    char src[] = "hallo";
     stringCopy(dest,src);
 }
 
@@ -48,27 +23,16 @@ void stringCopy(char* dest, const char* src) {
     int srcStrlen = strlen(src);
     int destStrlen = strlen(dest);
 
-    char* destcopy = dest;
-    char* srccopy = src;
+    char* destcopy = &(dest[0]);
+    char* srccopy = &(src[0]);
 
-    __CPROVER_printf("\ni: %d", i);
-    __CPROVER_printf("srcStrlen: %d", srcStrlen);
-    __CPROVER_printf("\n");
     while (i < srcStrlen) {
        *destcopy = *srccopy;
         i = i + 1;
         srccopy = srccopy + 1;
         destcopy = destcopy + 1;
     }
-    __CPROVER_printf("\n Destcopy offset: %d \n Srccopy offset: %d" ,__CPROVER_POINTER_OFFSET(destcopy), __CPROVER_POINTER_OFFSET(srccopy));
-    __CPROVER_printf("\n Destcopy strlen: %d \n Srccopy strlen: %d", destStrlen, srcStrlen);
-
-
-    *destcopy = *srccopy; //CBMC siger out of bounds. Men det er det ikke? 
-    //Den generer dette check
-    // OBJECT_SIZE(srccopy) >= 1ul + (unsigned long int)POINTER_OFFSET(srccopy) && POINTER_OFFSET(srccopy) >= 0l || DYNAMIC_OBJECT(srccopy)
-    //Som failer hvis src kun indeholder \0 
-
+    *destcopy = *srccopy;
 
 
     //Postcondition
@@ -77,10 +41,20 @@ void stringCopy(char* dest, const char* src) {
     }
 }
 
-//Run Command: cbmc stringCopy.c --bounds-check --pointer-check --unwind 5 --unwinding-assertions
+
+
+void stringCopy1(char* dest, const char* src) {
+
+    while(*src!='\0'){
+        *dest=*src;
+        dest++;
+        src++;
+    }
+    *dest='\0';
+}
+//Run Command: cbmc stringCopy.c --bounds-check --pointer-check 
 
 /*
     Reflections
         Loop invariant osv. virker ikke relevant
-        Fejler hvis src array længde = 1. (Out of bounds linje 67)
 */

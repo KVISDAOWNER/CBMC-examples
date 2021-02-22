9e5df04183b14887810bb7746ef691cae6b162da
Kristoffer Jensen <krijen16@student.aau.dk>
2021-02-22 10:16:52 +0100

    removed uncessary annotations after updated release

--- stringCopy/stringCopy.c
+++ stringCopy/stringCopy.c
@@ -7,32 +7,23 @@ void stringCopy1(char* dest, const char* src) ;
 int strlen2(const char* str) ;
 
 int main(int argc, char** argv){
+    //Source of nondetermism
     unsigned short int nondet_ushortint();
 
     unsigned short int size_dest = nondet_ushortint();
     __CPROVER_assume(5 > size_dest && size_dest > 0); //size_dest = 4, 3, 2, 1
     char dest[size_dest];
-    for(int j = 0; j < size_dest - 1; j++){
-        dest[j] = "a";
-    }
     dest[size_dest-1]='\0';
 
     unsigned short int size_src = nondet_ushortint(); 
-    __CPROVER_assume(5 > size_src && size_src > 1); //size_src = 4, 3, 2
+    __CPROVER_assume(5 > size_src && size_src > 0); //size_src = 4, 3, 2
     char src[size_src];
-    for(int j = 0; j < size_src - 1; j++){
-        src[j] = "b";
-    }
     src[size_src-1]='\0';
 
     for(int i=0; i<size_dest-1; i++){
         __CPROVER_assume(dest[i] != '\0');
     }
     __CPROVER_assume(size_dest >= size_src);
-
-    __CPROVER_printf("\nCASE");
-    __CPROVER_printf("Size of dest array: %d", (size_dest));
-    __CPROVER_printf("Size of src array: %d\n", (size_src));
 
     stringCopy(dest,src);
 }
@@ -51,18 +42,13 @@ void stringCopy(char* dest, const char* src) {
     char* destcopy = dest;
     char* srccopy = src;
 
-    __CPROVER_printf("\ni: %d", i);
-    __CPROVER_printf("srcStrlen: %d", srcStrlen);
-    __CPROVER_printf("\n");
+
     while (i < srcStrlen) {
        *destcopy = *srccopy;
         i = i + 1;
         srccopy = srccopy + 1;
         destcopy = destcopy + 1;
     }
-    __CPROVER_printf("\n Destcopy offset: %d \n Srccopy offset: %d" ,__CPROVER_POINTER_OFFSET(destcopy), __CPROVER_POINTER_OFFSET(srccopy));
-    __CPROVER_printf("\n Destcopy strlen: %d \n Srccopy strlen: %d", destStrlen, srcStrlen);
-
 
     *destcopy = *srccopy; //CBMC siger out of bounds. Men det er det ikke? 
     //Den generer dette check
